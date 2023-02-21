b18fd0196b7b8fd67a86b8ba4b36cf448ac3c8ad
Petr Porobov <porobov@users.noreply.github.com>
2023-02-21 12:46:26 +0400

    Tested. Receiver

---
+++ test/receiver.js
@@ -0,0 +1,31 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { setupTestEnvironment } = require("../src/deployer.js")
+const conf = require('../conf.js');
+
+// function to share deployment sequence between blocks of tests 
+// Solution from here https://stackoverflow.com/a/26111323 
+function makeSuite(name, tests) {
+  describe(name, function () {
+    before('setup', async () => {
+      ;[ownerGlobal, stranger] = await ethers.getSigners()
+      let env = await setupTestEnvironment({isDeployingMocks: conf.IS_DEPLOYING_MOCKS, isDeployingMinterAdapter: true})
+      wrapper = env.mehWrapper
+    })
+      this.timeout(142000)
+      tests();
+  });
+}
+
+makeSuite("Receiving", function () {
+
+  // Other tests make sure that wrapper does receive funds from WETH and OldMeh
+  it("Cannot receive funds from strangers", async function () {
+    let value = ethers.utils.parseEther("1.0")
+    await expect(stranger.sendTransaction({
+      to: wrapper.address,
+      value: value,
+    })).to.be.revertedWith(
+      "Receiver: Only receives from oldMEH or WETH")
+  })
+})
