f6c1d70bb6e352a6873da7e5bc2c90df3f329d58
Petr Porobov <porobov@users.noreply.github.com>
2023-02-23 11:38:52 +0400

    Hardhat bug bypass

--- contracts/Receiver.sol
+++ contracts/Receiver.sol
@@ -8,11 +8,13 @@ import "hardhat/console.sol";
 // Receives eth from WETH contract when converting weth to eth
 // Also receives eth from oldMEH when unwrapping blocks
 contract Receiver is UsingGlobals {
-
     receive() external payable {
         require((
             msg.sender == address(oldMeh) ||
-            msg.sender == address(WETH)),
-            "Receiver: Only receives from oldMEH or WETH");
+            // hardhat bug, uncomment the line below to fix flashloaner.js tests.
+            // address(WETH) returns same address, but in lower case - reason?
+            // msg.sender == 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 ||  
+            msg.sender == address(WETH)
+            ), "Receiver: Only receives from oldMEH or WETH");
     }
 }
