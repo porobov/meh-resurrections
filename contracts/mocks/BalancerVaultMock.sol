54f851e000afe75809c769274f05ab5cf58fd212
Petr Porobov <porobov@users.noreply.github.com>
2024-01-24 08:17:01 +0400

    Balancer mock compiles

--- contracts/mocks/BalancerVaultMock.sol
+++ contracts/mocks/BalancerVaultMock.sol
@@ -1,28 +1,34 @@
 pragma solidity ^0.8.0;
 
-import "./balancer-labs/solidity-utils/misc/IWETH.sol";
-import "../contracts/Flashloaner.sol:";
+import "../balancer-labs/solidity-utils/misc/IWETH.sol";
+import "../balancer-labs/solidity-utils/openzeppelin/IERC20.sol";
+import "../Flashloaner.sol";
 
 contract BalancerVaultMock {
 
     IWETH public WETH;
-    Flashloaner public wrapper;
 
-    constructor (wethAddress, wrapperAddress) {
+    constructor (address wethAddress) {
         WETH = IWETH(wethAddress);
-        wrapper = Flashloaner(wrapperAddress);
     }
 
-    // vault.flashLoan(this, tokens, amounts, userData);
-    function flashLoan(this, tokens, amounts, userData) {
-
+    function flashLoan(
+        address payable receiver,
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        bytes memory userData
+    ) external {
         // send WETH to wrapper
         uint256 loanAmount = amounts[0];
-        WETH.transfer(msg.sender, loanAmount);
+        WETH.transfer(receiver, loanAmount);
 
         // set fees to zero (as in real balancer)
         uint256[] memory feeAmounts = new uint256[](1);
         feeAmounts[0] = 0;
+
+        // init wrapper
+        Flashloaner wrapper = Flashloaner(receiver);
+
         // forward other params and call wrapper
         wrapper.receiveFlashLoan(tokens, amounts, feeAmounts, userData);
     }
