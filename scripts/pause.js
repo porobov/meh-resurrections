7d5b260ea2822c8a4067b69d98af74655dc072f2
Petr Porobov <porobov@users.noreply.github.com>
2024-02-07 10:57:36 +0400

    Emergency pause script

---
+++ scripts/pause.js
@@ -0,0 +1,21 @@
+// emergency pause MEH
+// npx hardhat run scripts/pause.js --network mainnet
+const { ethers } = require("hardhat")
+const { ProjectEnvironment, Deployer } = require("../src/deployer.js")
+
+async function pause() {
+    ;[owner] = await ethers.getSigners()
+    const exEnv = new ProjectEnvironment(owner)
+    const deployer = new Deployer(exEnv, {})
+    console.log("Pausing MEH on chain ID:", exEnv.chainID)
+    await deployer.initialize()
+    await deployer.pauseMeh2016()
+    console.log("MEH paused successfully")
+}
+
+pause()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
