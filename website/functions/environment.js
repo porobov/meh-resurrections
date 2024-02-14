5c9af61688e744f384fbe5ccbca6eb770314500e
Petr Porobov <porobov@users.noreply.github.com>
2024-02-14 10:14:59 +0400

    Removed env fetch functions
    Will move to React or Next anyway. Env will be there.

--- website/functions/environment.js
+++
@@ -1,7 +0,0 @@
-export function onRequest(context) {
-	if (context.env.ENVIRONMENT === 'development') {
-		return new Response('This is a local environment!');
-	} else {
-		return new Response('This is a live environment');
-	}
-}
