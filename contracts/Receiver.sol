12724b5305be47ef1930bf2734b660b39394a28f
Petr Porobov <porobov@users.noreply.github.com>
2024-02-14 11:08:54 +0400

    Receiver better comments on hardhat bugs

--- contracts/Receiver.sol
+++ contracts/Receiver.sol
@@ -9,12 +9,19 @@ import "hardhat/console.sol";
 // Also receives eth from oldMEH when unwrapping blocks
 contract Receiver is UsingGlobals {
     receive() external payable {
+        // the console.log's here are meant to fix some hardhat bug
+        // turn them on for testing locally with mocks (no hardfork)
+        // console.log("....Receiver. msg.sender", msg.sender);
+        // console.log("....Receiver. address(WETH)", address(WETH));
         require((
             msg.sender == address(oldMeh) ||
             // hardhat bug, uncomment the line below to fix flashloaner.js tests.
             // address(WETH) returns same address, but in lower case - reason?
+            // TODO may be harmfull as this address private key is public (hardhat)
+            // comment for production
             // msg.sender == 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 ||  
             msg.sender == address(WETH)
             ), "Receiver: Only receives from oldMEH or WETH");
+        // console.log("Flashloaner contract eth balance", address(this).balance);
     }
 }
