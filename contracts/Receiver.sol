562eb0d41c5e0a4ece6e6e8e0819a498ff5de089
Petr Porobov <porobov@users.noreply.github.com>
2024-02-17 14:05:05 +0400

    Checkpoint. All tests work. Including Flashloaner
    This is a checkpoint to report issue about hardhat bug

--- contracts/Receiver.sol
+++ contracts/Receiver.sol
@@ -17,9 +17,10 @@ contract Receiver is UsingGlobals {
             msg.sender == address(oldMeh) ||
             // hardhat bug, uncomment the line below to fix flashloaner.js tests.
             // address(WETH) returns same address, but in lower case - reason?
-            // TODO may be harmfull as this address private key is public (hardhat)
-            // comment for production
-            // msg.sender == 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 ||  
+            // safe to use real weth address in production
+            // but may be harmfull if using hardhat's mocked address
+            // TODO check before production deployment
+            msg.sender == 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 ||  
             msg.sender == address(WETH)
             ), "Receiver: Only receives from oldMEH or WETH");
         // console.log("Flashloaner contract eth balance", address(this).balance);
