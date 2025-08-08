da9c3a6d4b23166b3681445cc32cdfd6eaa65d6f
Petr Porobov <porobov@users.noreply.github.com>
2025-08-08 13:30:05 +0400

    Clearer admin balance script logic and output

--- scripts/adminBalance.js
+++ scripts/adminBalance.js
@@ -1,12 +1,22 @@
-const { getRealMehAdminSigner, getFormattedBalance } = require('../src/tools.js');
+const { getFormattedBalance } = require('../src/tools.js');
+const { getDeployer } = require('../src/deployer.js');
 
+// balances and addresses for mehAdmin and operator (see if they are the same)
 async function main() {
   try {
-    const admin = await getRealMehAdminSigner();
-    const address = admin.address;
-    const balance = await getFormattedBalance(address);
-    console.log(`Admin address: ${address}`);
-    console.log(`ETH balance: ${balance}`);
+    const deployer = await getDeployer();
+    await deployer.initialize();
+    
+    const operatorAddress = deployer.exEnv.operatorWallet.address;
+    const mehAdminAddress = deployer.exEnv.mehAdminAddress;
+    const operatorBalance = await getFormattedBalance(operatorAddress);
+    const mehAdminBalance = await getFormattedBalance(mehAdminAddress);
+    
+    console.log(`Operator address: ${operatorAddress}`);
+    console.log(`Operator ETH balance: ${operatorBalance} ETH`);
+    console.log(`meh Admin address: ${mehAdminAddress}`);
+    console.log(`meh Admin ETH balance: ${mehAdminBalance} ETH`);
+    
   } catch (err) {
     console.error('Error:', err.message || err);
     process.exit(1);
