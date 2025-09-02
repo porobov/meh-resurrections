1da1eca60a912e0eafb10f6dcade028178f8b24e
Petr Porobov <porobov.p@gmail.com>
2025-09-02 16:16:23 +0400

    Now can set new price for main net too.

--- scripts/setNewPrice.js
+++ scripts/setNewPrice.js
@@ -3,25 +3,28 @@
 const { ethers } = require("hardhat")
 const { ProjectEnvironment, Deployer } = require("../src/deployer.js")
 const chalk = require('chalk')
+const { getConfigChainID, getRealMehAdminSigner } = require("../src/tools.js")
 
-const NEW_PRICE_IN_ETH = "0.001"
+const NEW_PRICE_IN_ETH = "0.1"
 
 const newPrice = ethers.parseEther(NEW_PRICE_IN_ETH)
 async function setNewPrice() {
-    ;[owner] = await ethers.getSigners()
-    const exEnv = new ProjectEnvironment(owner)
-    const deployer = new Deployer(exEnv, {})
-    console.log("Setting new price", exEnv.chainID)
-    if (exEnv.chainID == 11155111) {
-      await deployer.initialize()
-      await deployer.unpauseMeh2016()
-      const tx = await deployer.mehWrapper.adminSetPrice(newPrice)
-      await deployer.pauseMeh2016()
-      console.log(chalk.gray("Tx:", tx?.hash))
-      console.log("New price is set to", NEW_PRICE_IN_ETH)
-    } else {
-      console.log(chalk.red("Can only set new price for testnet. Modify script for mainnet"))
-    }
+  let owner;
+  if (getConfigChainID() === 1) {
+    owner = await getRealMehAdminSigner();
+  } else {
+    [owner] = await ethers.getSigners();
+  }
+  const exEnv = new ProjectEnvironment(owner)
+  const deployer = new Deployer(exEnv, {})
+  console.log("Setting new price", exEnv.chainID)
+
+  await deployer.initialize()
+  await deployer.unpauseMeh2016()
+  const tx = await deployer.mehWrapper.adminSetPrice(newPrice)
+  await deployer.pauseMeh2016()
+  console.log(chalk.gray("Tx:", tx?.hash))
+  console.log("New price is set to", NEW_PRICE_IN_ETH)
 }
 
 setNewPrice()
